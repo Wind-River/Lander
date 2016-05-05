e4dc041467b0ed17d7a283b31cef9a41914bb18b
Pauline Cheung <hapyeyes@hotmail.com>
2016-05-05 03:15:47 -0700

    move directory structure aroud; update README to md format; add arduino_edison.py;

---
+++ source/edison/README.md
@@ -0,0 +1,19 @@
+#Edison Board Set up
+
+ * Startup script is /etc/init.d/rocketLanderStart
+ * All scripts have been copied to /home/root on the edison board
+ * u:p root:rocketLander
+
+##Wifi
+ * Will need to be reconfigured, atm only works at WIND 
+ * configure_edison --wifi
+
+##Bluetooth
+ * Will show up as RocketLander in bluetooth list
+ * Default bluetooth pin is 0505
+ * Set up to start up automatically
+
+##Arduino to Edison Serial
+ * Set up to start up automatically
+
+
---
+++ source/edison/arduino_edison.py
@@ -0,0 +1,132 @@
+import serial
+import time
+import os
+import re
+import json
+import sys
+import subprocess
+
+SERIALPORT = "/dev/ttyMFD1"
+BAUDRATE = 9600
+
+ser = serial.Serial(SERIALPORT, BAUDRATE)
+
+ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+
+ser.parity = serial.PARITY_NONE #set parity check: no parity
+
+ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+
+ser.timeout = None            #timeout block read
+
+ser.xonxoff = False     #disable software flow control
+
+ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+
+ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+
+ser.writeTimeout = 0     #timeout for write
+
+if len(sys.argv) > 1:
+    print "DEBUG MODE---->";
+    DEBUG = True
+else:
+    DEBUG = False
+
+def processMessage(message):
+    # tear it apart
+    # a2e:highscore?;
+    # a2e:name=____,score-_____;
+
+    matchObj = re.match(r'^a2e:(.*);', message)
+    if matchObj is None:
+        raise Exception("processMessage Exception: message not expected, do nothing");
+
+    mess = matchObj.group(1)
+    test = 'highscore?'
+    if DEBUG:
+        print mess
+        test = 'uname'
+    if mess == test:
+        return ''
+    else:
+        timestamp = time.time()
+        # test string
+        if DEBUG:
+            mess = "name=WonderBread,score=00000"
+        matchObj = re.match(r'name=(.*),score=(.*)', mess);
+        if matchObj is None:
+            raise Exception("processMessage Exception: message format not expected, do nothing")
+        name = matchObj.group(1)
+        score = matchObj.group(2)
+
+        # construct the json string send
+        # email is primary key and needs to be unqiue
+        # we'll just make one up, so its like old video games, unique entry
+        # for each score
+        jsonString = '[{"name":"' + name + '","lander_score":"'+ score +'","email":"' + name + str(timestamp) + '@rocketLander.com"}]'
+        print jsonString
+        return jsonString
+
+HANDSHAKE = "e2a:ack;\r\n"
+print 'Starting Up Serial Monitor'
+
+try:
+    ser.open()
+except Exception, e:
+    print "error open serial port: " + str(e)
+#    exit()
+
+if ser.isOpen():
+
+    try:
+        ser.flushInput() #flush input buffer, discarding all its contents
+        ser.flushOutput()#flush output buffer, aborting current output
+
+        ser.write(HANDSHAKE)
+        time.sleep(0.5)
+
+        while True:
+
+            response = ser.readline()
+
+            print("read data: " + response)
+            try:
+                jsonString = processMessage(response)
+            except Exception, e:
+                print str(e)
+                continue;
+            try:
+                if jsonString:
+                    if DEBUG:
+                        print "Submit High Score"
+                    #submit high score
+                    data = json.loads(jsonString)
+                    print json.dumps(data, indent=4, sort_keys=True)
+                    if not DEBUG:
+                        os.spawnlp(os.P_NOWAIT, "/usr/bin/python", "python", "rocketLanderRest.py", jsonString)
+
+                    ser.write(HANDSHAKE)
+                else:
+                    # get the high score
+                    if DEBUG:
+                        print "Get high score"
+                    if not DEBUG:
+                        output = subprocess.check_output(["/usr/bin/python /home/root/rocketLander.py"], shell=True);
+              
+                    ser.write(HANDSHAKE)
+            except Exception, e:
+                print "unable to spawn rest api call...: " + str(e)
+
+        ser.close()
+
+    except Exception, e:
+        print "error communicating...: " + str(e)
+
+else:
+    print "cannot open serial port "
+
+
+
+
+    
